f0c213eed14e95c2a6b46d631c955ae3ad640b09
geetanjali44 <geetanjaligajjana@gmail.com>
2026-07-08 11:16:10 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://ubmbfvibhpdjkdmlmxab.supabase.co/";
+const SUPABASE_ANON_KEY = "sb_publishable_xDEiKkBSUxBt908dh7awOg_1ouGWrY8";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
